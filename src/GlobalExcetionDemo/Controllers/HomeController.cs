bdd473b9b5a53cd72204ce3815b8e21a625c8403
qinxiaowen <453000697@qq.com>
2016-12-20 17:49:17 +0800

    1

---
+++ src/GlobalExcetionDemo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GlobalExcetionDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HomeController>();
+        }
+        public IActionResult Index()
+        {
+            throw new Exception("global exception");
+            _logger.LogInformation("123123");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/GlobalExcetionDemo/YourCustomFilter .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalExcetionDemo
+{
+    public class YourCustomFilter : ActionFilterAttribute, IExceptionFilter
+    {
+        private readonly ILogger<YourCustomFilter> _logger;
+        public YourCustomFilter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<YourCustomFilter>();
+        }
+        //public YourCustomFilter
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception.ToString());
+        }
+    }
+}
