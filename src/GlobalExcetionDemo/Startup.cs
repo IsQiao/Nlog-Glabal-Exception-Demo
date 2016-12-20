fcae571f3c01d4fd5a5b764b5a4ca52e76c80c26
qinxiaowen <453000697@qq.com>
2016-12-20 18:01:23 +0800

    12

--- src/GlobalExcetionDemo/Startup.cs
+++ src/GlobalExcetionDemo/Startup.cs
@@ -28,7 +28,7 @@ namespace GlobalExcetionDemo
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
